8493beb670b44994d66dd24455c3f866fa23cb07
Rohit Singh <rohitskt115@gmail.com>
2023-03-30 13:40:37 +0530

    updated for prod

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGODB_URI,
+    cookieKey: process.env.COOKIE_KEY,
+    sripePublishableKey: process.env.SRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.SRIPE_SECRET_KEY,
+  };
+  
